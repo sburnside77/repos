d369a1a1af905ef3fedff0c6f9517d78b7d92db4
Scott Burnside <sburnside77@yahoo.com>
2018-08-30 10:31:54 -0500

    Initial Version

---
+++ LUISGetIntent/LUISGetIntent/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net.Http;
+
+/*
+
+	You can use the authoring key instead of the endpoint key. 
+	The authoring key allows 1000 endpoint queries a month.
+
+*/
+
+namespace LUISGetIntent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MakeRequest();
+            Console.WriteLine("Hit ENTER to exit...");
+            Console.ReadLine();
+        }
+
+        static async void MakeRequest()
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // This app ID is for a public sample app that recognizes requests to turn on and turn off lights
+            var luisAppId = "54210c7c-05c0-4b49-ba6e-804722195e34";
+            var endpointKey = "59027ce6be664e1aad5dddefabf4a7c3";
+
+            // The request header contains your subscription key
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", endpointKey);
+
+            // The "q" parameter contains the utterance to send to LUIS
+            queryString["q"] = "turn on the left light";
+
+            // These optional request parameters are set to their default values
+            queryString["timezoneOffset"] = "0";
+            queryString["verbose"] = "false";
+            queryString["spellCheck"] = "false";
+            queryString["staging"] = "false";
+
+            var endpointUri = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/" + luisAppId + "?" + queryString;
+            var response = await client.GetAsync(endpointUri);
+
+            var strResponseContent = await response.Content.ReadAsStringAsync();
+
+            // Display the JSON result from LUIS
+            Console.WriteLine(strResponseContent.ToString());
+
+        }
+    }
+}
